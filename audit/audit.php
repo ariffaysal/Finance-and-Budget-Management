7e13e2f37834a2b61a9f3b6b7e65dfdab73f790e
ariffaysal <ariffaysal001@gmai.com>
2025-09-06 10:54:00 +0600

    cmt

--- audit/audit.php
+++ audit/audit.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Auditor</title>
-    <link rel="stylesheet" href="audit.css" type="text/css">
+    <link rel="stylesheet" href="../Audit/audit.css" type="">
 </head>
 
 <body>
